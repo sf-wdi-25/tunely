ab2f636f7565e8e47dc1e9e4c6d89ff3cf1c98dd
Noah Wimmer <nwimmer123@yahoo.com>
2015-12-08 15:56:22 -0800

    finished step3

--- models/album.js
+++ models/album.js
@@ -1,2 +1,11 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
+
+var AlbumSchema = new mongoose.Schema({
+	artistName: String,
+	name: String,
+	releaseDate: String,
+	genre: [ String ]
+});
+
+var Album = moongose.model('Album', AlbumSchema);
