e6e69ab1af7bdbf9d22df8aba6f12b0287e2e18a
isangieri <isabelleangieri@gmail.com>
2015-12-08 13:12:43 -0800

    rendered all albums to html

--- public/js/app.js
+++ public/js/app.js
@@ -39,7 +39,9 @@ sampleAlbums.push({
 $(document).ready(function() {
   console.log('app.js loaded!');
 
-renderAlbum(sampleAlbums[0]);
+sampleAlbums.forEach( function ( element, index) {
+  renderAlbum(element);
+});
 
 
 
@@ -49,47 +51,48 @@ renderAlbum(sampleAlbums[0]);
 function renderAlbum(album) {
   console.log('rendering album:', album);
 
-  var albumHtml =
-  "        <!-- one album -->" +
-  "        <div class='row album' data-album-id='" + album.id + "'>" +
-  "          <div class='col-md-10 col-md-offset-1'>" +
-  "            <div class='panel panel-default'>" +
-  "              <div class='panel-body'>" +
-  "              <!-- begin album internal row -->" +
-  "                <div class='row'>" +
-  "                  <div class='col-md-3 col-xs-12 thumbnail album-art'>" +
-  "                     <img src='" + "http://placehold.it/400x400'" +  " alt='album image'>" +
-  "                  </div>" +
-  "                  <div class='col-md-9 col-xs-12'>" +
-  "                    <ul class='list-group'>" +
-  "                      <li class='list-group-item'>" +
-  "                        <h4 class='inline-header'>Album Name:</h4>" +
-  "                        <span class='album-name'>" + album.name + "</span>" +
-  "                      </li>" +
-  "                      <li class='list-group-item'>" +
-  "                        <h4 class='inline-header'>Artist Name:</h4>" +
-  "                        <span class='artist-name'>" + album.artistName + "</span>" +
-  "                      </li>" +
-  "                      <li class='list-group-item'>" +
-  "                        <h4 class='inline-header'>Released date:</h4>" +
-  "                        <span class='album-releaseDate'>" + album.releaseDate + "</span>" +
-  "                      </li>" +
-  "                    </ul>" +
-  "                  </div>" +
-  "                </div>" +
-  "                <!-- end of album internal row -->" +
+    var albumHtml =
+    "        <!-- one album -->" +
+    "        <div class='row album' data-album-id='" + album.id + "'>" +
+    "          <div class='col-md-10 col-md-offset-1'>" +
+    "            <div class='panel panel-default'>" +
+    "              <div class='panel-body'>" +
+    "              <!-- begin album internal row -->" +
+    "                <div class='row'>" +
+    "                  <div class='col-md-3 col-xs-12 thumbnail album-art'>" +
+    "                     <img src='" + "http://placehold.it/400x400'" +  " alt='album image'>" +
+    "                  </div>" +
+    "                  <div class='col-md-9 col-xs-12'>" +
+    "                    <ul class='list-group'>" +
+    "                      <li class='list-group-item'>" +
+    "                        <h4 class='inline-header'>Album Name:</h4>" +
+    "                        <span class='album-name'>" + album.name + "</span>" +
+    "                      </li>" +
+    "                      <li class='list-group-item'>" +
+    "                        <h4 class='inline-header'>Artist Name:</h4>" +
+    "                        <span class='artist-name'>" + album.artistName + "</span>" +
+    "                      </li>" +
+    "                      <li class='list-group-item'>" +
+    "                        <h4 class='inline-header'>Released date:</h4>" +
+    "                        <span class='album-releaseDate'>" + album.releaseDate + "</span>" +
+    "                      </li>" +
+    "                    </ul>" +
+    "                  </div>" +
+    "                </div>" +
+    "                <!-- end of album internal row -->" +
 
-  "              </div>" + // end of panel-body
+    "              </div>" + // end of panel-body
 
-  "              <div class='panel-footer'>" +
-  "              </div>" +
+    "              <div class='panel-footer'>" +
+    "              </div>" +
 
-  "            </div>" +
-  "          </div>" +
-  "          <!-- end one album -->";
+    "            </div>" +
+    "          </div>" +
+    "          <!-- end one album -->";
 
   // render to the page with jQuery
   $('#albums').append(albumHtml);
+
 }
 
 });
