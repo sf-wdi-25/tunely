149904591cf4b35df1defb768081f0acc4a6984f
anonym0us3 <nathan.shayevich@gmail.com>
2015-12-09 01:16:36 -0800

    sprint 2, step 2 complete

--- public/js/app.js
+++ public/js/app.js
@@ -6,36 +6,6 @@
  */
 
 
-/* hard-coded data! */
-var sampleAlbums = [];
-sampleAlbums.push({
-             artistName: 'Ladyhawke',
-             name: 'Ladyhawke',
-             releaseDate: '2008, November 18',
-             genres: [ 'new wave', 'indie rock', 'synth pop' ]
-           });
-sampleAlbums.push({
-             artistName: 'The Knife',
-             name: 'Silent Shout',
-             releaseDate: '2006, February 17',
-             genres: [ 'synth pop', 'electronica', 'experimental' ]
-           });
-sampleAlbums.push({
-             artistName: 'Juno Reactor',
-             name: 'Shango',
-             releaseDate: '2000, October 9',
-             genres: [ 'electronic', 'goa trance', 'tribal house' ]
-           });
-sampleAlbums.push({
-             artistName: 'Philip Wesley',
-             name: 'Dark Night of the Soul',
-             releaseDate: '2008, September 12',
-             genres: [ 'piano' ]
-           });
-/* end of hard-coded data */
-
-
-
 
 $(document).ready(function() {
   console.log('app.js loaded!');
@@ -44,6 +14,15 @@ $(document).ready(function() {
       renderAlbum(album);
     });
   });
+
+
+  $('#album-form form').on('submit', function (event) {
+    event.preventDefault();
+    var formData = $(this).serialize();
+    console.log('formData', formData);
+    $(this).trigger("reset");
+  });
+  
 });
 
 
