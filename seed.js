86fee000bfccdf02c299daae5cf85bb028861c74
jroers <joe.roers@gmail.com>
2015-12-08 23:36:10 -0800

    Completed sprints 1-4

--- seed.js
+++ seed.js
@@ -4,8 +4,49 @@
 var db = require("./models");
 
 var albumsList =[
-  // put data here!
+{
+  artistName: 'Nine Inch Nails',
+  name: 'The Downward Spiral',
+  releaseDate: '1994, March 8',
+  genres: [ 'industrial', 'industrial metal' ]
+}, {
+  artistName: 'Metallica',
+  name: 'Metallica',
+  releaseDate: '1991, August 12',
+  genres: [ 'heavy metal' ]
+}, {
+  artistName: 'The Prodigy',
+  name: 'Music for the Jilted Generation',
+  releaseDate: '1994, July 4',
+  genres: [ 'electronica', 'breakbeat hardcore', 'rave', 'jungle' ]
+}, {
+  artistName: 'Johnny Cash',
+  name: 'Unchained',
+  releaseDate: '1996, November 5',
+  genres: [ 'country', 'rock' ]
+}];
+
+var sampleSongs = [
+{    name: 'Swamped',
+     trackNumber: 1
+}, { name: "Heaven's a Lie",
+     trackNumber: 2
+}, { name: 'Daylight Dancer',
+     trackNumber: 3
+}, { name: 'Humane',
+     trackNumber: 4
+}, { name: 'Self Deception',
+     trackNumber: 5
+}, { name: 'Aeon',
+     trackNumber: 6
+}, { name: 'Tight Rope',
+     trackNumber: 7
+}
 ];
+
+albumsList.forEach(function (element) {
+	element.songs = sampleSongs;
+});
 
 db.Album.remove({}, function(err, albums){
 
--- server.js
+++ server.js
@@ -5,45 +5,21 @@ var express = require('express');
 // generate a new express app and call it 'app'
 var app = express();
 
+var mongoose = require("mongoose");
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
+
+
 
 /************
  * DATABASE *
  ************/
-
+var db = require("./models");
 /* hard-coded data */
-var albums = [];
-albums.push({
-              _id: 132,
-              artistName: 'Nine Inch Nails',
-              name: 'The Downward Spiral',
-              releaseDate: '1994, March 8',
-              genres: [ 'industrial', 'industrial metal' ]
-            });
-albums.push({
-              _id: 133,
-              artistName: 'Metallica',
-              name: 'Metallica',
-              releaseDate: '1991, August 12',
-              genres: [ 'heavy metal' ]
-            });
-albums.push({
-              _id: 134,
-              artistName: 'The Prodigy',
-              name: 'Music for the Jilted Generation',
-              releaseDate: '1994, July 4',
-              genres: [ 'electronica', 'breakbeat hardcore', 'rave', 'jungle' ]
-            });
-albums.push({
-              _id: 135,
-              artistName: 'Johnny Cash',
-              name: 'Unchained',
-              releaseDate: '1996, November 5',
-              genres: [ 'country', 'rock' ]
-            });
-
-
 
 /**********
  * ROUTES *
@@ -70,6 +46,64 @@ app.get('/api', function api_index (req, res){
     endpoints: [
       {method: "GET", path: "/api", description: "Describes available endpoints"}
     ]
+  });
+});
+
+app.get('/api/albums', function fetchAlbums (req, res) {
+  db.Album.find({}, function (err, success) {
+    if (err) { return console.log(err); }
+    res.send(success);
+  });
+});
+
+app.get('/api/albums/:album_id', function getOneAlbum (req, res) {
+  db.Album.findById(req.params.album_id, function (err, success) {
+    if (err) { return console.log(err); }
+    res.send(success);
+  });
+});
+
+app.get('/api/albums/:album_id/songs', function getAlbumSongs (req, res) {
+  db.Album.findById(req.params.album_id, function (err, success) {
+    if (err) { return console.log(err); }
+    res.json(success.songs);
+  });
+});
+
+// app.get('/api/albums/:album_id/songs/:id', function getAlbumSongs (req, res) {
+//   db.Album.findById(req.params.album_id, function (err, album) {
+//     if (err) { return console.log(err); }
+//     db.Song.findById(req.params.id, function (err, song) {
+//       if (err) { return console.log(err); }
+//       console.log(song);
+//     });
+//   });
+// });
+
+app.post('/api/albums', function newAlbum (req, res) {
+  db.Album.create(req.body, function (err, success) {
+    if (err) { return console.log(err); }
+    res.send(success);
+  });
+});
+
+app.post('/api/albums/:album_id/songs', function newSong (req, res) {
+  db.Album.findById(req.params.album_id, function (err, success) {
+    if (err) { return console.log(err); }
+    db.Song.create(req.body, function (err, newSong) {
+      if (err) { return console.log(err); }
+      success.songs.push(newSong);
+      success.save(function (err, savedSong) {
+        res.json(success);
+      });
+    });
+  });
+});
+
+app.delete('/api/albums/:id', function (req, res) {
+  db.Album.remove({_id: req.params.id}, function (err, success) {
+    if (err) { return console.log(err); }
+    res.json({status: 200});
   });
 });
 
