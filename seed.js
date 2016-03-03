093dbf6040d65060fc4edf208896496c914a7835
nscalf561 <scalf.nicholas@gmail.com>
2016-03-03 14:02:35 -0800

    Adds Seed and Database

--- seed.js
+++ seed.js
@@ -1,19 +1,51 @@
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
 
-var db = require("./models");
+var db = require("./models/");
+var Album = require('./models/album');
+var mongoose = require('mongoose');
 
 var albumsList =[
   // put data here!
+	{
+    artistName: 'Nine Inch Nails',
+    name: 'The Downward Spiral',
+    releaseDate: '1994, March 8',
+    genres: [ 'industrial', 'industrial metal' ]
+  },
+	{
+    artistName: 'Metallica',
+    name: 'Metallica',
+    releaseDate: '1991, August 12',
+    genres: [ 'heavy metal' ]
+  },
+	{
+    artistName: 'The Prodigy',
+    name: 'Music for the Jilted Generation',
+    releaseDate: '1994, July 4',
+    genres: [ 'electronica', 'breakbeat hardcore', 'rave', 'jungle' ]
+  },
+	{
+    artistName: 'Johnny Cash',
+    name: 'Unchained',
+    releaseDate: '1996, November 5',
+    genres: [ 'country', 'rock' ]
+  }
 ];
 
-db.Album.remove({}, function(err, albums){
+Album.remove({}, function(err, albums){
 
-  db.Album.create(albumsList, function(err, albums){
-    if (err) { return console.log('ERROR', err); }
+  Album.create(albumsList, function(err, albums){
+    if (err) { 
+    	return console.log('ERROR', err); 
+    }
+    else{
     console.log("all albums:", albums);
     console.log("created", albums.length, "albums");
     process.exit();
+  	mongoose.connection.close();
+    }
   });
 
 });
+
