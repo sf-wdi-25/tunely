161758139183bff9a036a4cbb440984cbc5ed607
mcarter78 <sjsharksftw@gmail.com>
2016-03-04 15:30:25 -0800

    builds index view with image carousel

--- public/js/app.js
+++ public/js/app.js
@@ -15,6 +15,39 @@
 $(document).ready(function() {
   console.log('app.js loaded!');
   var app = new App();
+
+  $('.multiple-items').slick({
+    infinite: true,
+    dots: true,
+    speed: 800,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 960,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 780,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+  });
 });
 
 function App () {
