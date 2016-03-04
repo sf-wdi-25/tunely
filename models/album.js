d4841472ecc7edd7fb1c60913ce77440dbb3bee9
lenayee <lenayee07@gmail.com>
2016-03-04 09:30:52 -0800

    adds seed data & modified new form

--- models/album.js
+++ models/album.js
@@ -4,7 +4,8 @@ var mongoose = require("mongoose");
 var albumSchema = mongoose.Schema({
   artistName: String,
   name: String,
-  releaseDate: String
+  releaseDate: String,
+  photoUrl: String
 });
 
 module.exports = mongoose.model('Album', albumSchema);
